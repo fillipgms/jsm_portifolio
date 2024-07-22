99d49691066a92140144cb7f19063e6d164b011f
Fillip Mangia <fillipgms@gmail.com>
2024-07-22 17:11:55 -0300

    first commit

---
+++ components/Grid.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+
+const Grid = () => {
+    return (
+        <section>
+            <BentoGrid className="w-full py-20">
+                {gridItems.map(
+                    ({
+                        id,
+                        title,
+                        description,
+                        className,
+                        img,
+                        imgClassName,
+                        titleClassName,
+                        spareImg,
+                    }) => (
+                        <BentoGridItem
+                            id={id}
+                            key={id}
+                            title={title}
+                            description={description}
+                            className={className}
+                            img={img}
+                            imgClassname={imgClassName}
+                            titleClassName={titleClassName}
+                            spareImg={spareImg}
+                        />
+                    )
+                )}
+            </BentoGrid>
+        </section>
+    );
+};
+
+export default Grid;
